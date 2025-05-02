b494a53093b57d6118abb7ec0fec526965cc27a3
GGengX <14792202+ggengx@user.noreply.gitee.com>
2025-05-02 20:55:47 +0800

    merge get class stu list

--- src/main/java/com/se/constant/ClassEntityConstant.java
+++ src/main/java/com/se/constant/ClassEntityConstant.java
@@ -2,6 +2,7 @@ package com.se.constant;
 
 public class ClassEntityConstant {
 
+    // 班级代码位数
     public static final Integer CLASS_CODE_PLACE_NUM = 9;
     public static final String CLASS_NAME_EXISTS = "班级名称已经存在";
     public static final String CLASS_NOT_EXISTS = "班级不存在";
